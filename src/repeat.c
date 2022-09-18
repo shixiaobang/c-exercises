a309b254bf70e5cc212e6ae08248d88fe3089648
shixiaobang <xiaobang.shi@outlook.com>
2022-09-18 12:25:59 +0800

    refactor

--- src/repeat.c
+++ src/repeat.c
@@ -3,13 +3,20 @@
 int main(int argc, char const *argv[])
 {
     int array[3];
-    for (int i = 0; i < 3; i++)
+    int length;
+    length = sizeof(array) / sizeof(int);
+
+    int times;
+    times = 2;
+
+    for (int i = 0; i < length; i++)
     {
         scanf("%d", &array[i]);
     }
-    for (int i = 0; i < 3; i++)
+
+    for (int i = 0; i < length; i++)
     {
-        for (int j = 0; j < 2; j++)
+        for (int j = 0; j < times; j++)
         {
             printf("%d ", array[i]);
         }
--- src/reverse.c
+++ src/reverse.c
@@ -4,17 +4,20 @@ int main(int argc, char const *argv[])
 {
     int array[5];
 
-    for (int i = 0; i < 5; i++)
+    int length;
+    length = sizeof(array) / sizeof(int);
+
+    for (int i = 0; i < length; i++)
     {
         scanf("%d", &array[i]);
     }
 
-    for (int i = 4; i > -1; i--)
+    for (int i = length - 1; i > -1; i--)
     {
         printf("%d ", array[i]);
     }
 
     printf("\n");
-    
+
     return 0;
 }
